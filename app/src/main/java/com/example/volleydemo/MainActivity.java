7ed687bca20e97ebcf47bde076c7db9678401266
nietof <nietof@google.com>
2020-03-10 17:32:04 -0700

    Changed to OMDB api, and added more JSON data to the text view

--- app/src/main/java/com/example/volleydemo/MainActivity.java
+++ app/src/main/java/com/example/volleydemo/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     RequestQueue requestQueue;  //STEP 1
     private TextView dataTextView;
-    String data = "";
+    String dataToDisplay = "";
+    private final String API_KEY = "388d5f18";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +40,13 @@ public class MainActivity extends AppCompatActivity {
         final int max = 20;
         final int random = new Random().nextInt((max - min) + 1) + min;
 
-
         requestQueue = Volley.newRequestQueue(this); //STEP 2
 
-        String url = "https://swapi.co/api/people/"+random+"/?format=json";
+        //String url = "https://swapi.co/api/planets/"+random+"/?format=json";
+
+        //*************************************
+
+        String url = "https://www.omdbapi.com/?apikey="+ API_KEY+"&t=Toy+Story+4&y=2019";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
 
@@ -50,22 +54,35 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        Log.d("response", " " + response );
-
                         try{
 
-                            data = response.getString("name");
+                            String title = response.getString("Title");
+                            String genre = response.getString("Genre");
+                            String director = response.getString("Director");
 
-                            Log.d("data", "Name : " + data);
 
-                            dataTextView.setText(data.toString());
+
+
+
+
+
+                            /*******************************************************************************************************
+                              The http://www.omdbapi.com/ database has the ratings stored in a JSON Object inside a JSON Array.
+                              Rotten Tomatoes is the second object in the JSON Array - index 1.
+                              Each rating has a value for "Value", so we grab  that at the end of the statement.
+                             *********************************************************************************************************/
+                            String rottenRating = response.getJSONArray("Ratings").getJSONObject(1).getString("Value");
+
+                            dataToDisplay = title +
+                                    "\n Directed by " + director +
+                                    "\n genre: " + genre +
+                                    "\n RT: " + rottenRating;
+
+                            dataTextView.setText(dataToDisplay.toString());
 
                         }catch (JSONException e){
                             e.printStackTrace();
-
                         }
-
-
                     }
                 },
                 new Response.ErrorListener() {
@@ -76,14 +93,12 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-
          requestQueue.add(jsonObjectRequest);
 
 
     }
 
-
-    //TODO Write a method that takes in a url and a key from the API and returns a value
+    //TODO Complete this fetchData() method that takes in a url and a key from the API and returns a value
 
 
     public void fetchData(String url, String key){
@@ -94,25 +109,17 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-
                         Log.e("response" , ""+ response);
-
-
 
                         try{
 
-                            data = response.getString("name");
-
-                            Log.d("data", "Name : " + data);
-
-                            dataTextView.setText(data.toString());
+                            dataToDisplay = response.getString("Something");
 
 
                         }catch(JSONException e){
 
                             e.printStackTrace();
                         }
-
 
                     }
                 },
